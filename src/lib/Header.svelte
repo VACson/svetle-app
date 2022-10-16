2817810902bce8dbe6256796c996ecdb4f034a64
VACson <memedealer1337@gmail.com>
2022-10-16 13:22:07 +0300

    add header

---
+++ src/lib/Header.svelte
@@ -0,0 +1,49 @@
+<script>
+  import Background from '../assets/Bg.png';
+  import Burger from '../assets/burgermenu.svg';
+  import Facebook from '../assets/icons/facebook.svg';
+  import Instagram from '../assets/icons/instagram.svg';
+  import Be from '../assets/icons/Be.svg';
+  import Twitter from '../assets/icons/twitter.svg';
+  import Pinterest from '../assets/icons/pinterest.svg';
+</script>
+
+<header class="header">
+  <img class="header__img" src="{Background}" alt="" />
+  <div class="header__section">
+    <nav class="nav">
+      <div class="header__logo">
+        <div class="header__logo__text">Portfo.</div>
+        <div class="header__logo__circle"></div>
+      </div>
+      <img class="nav__burger" src="{Burger}" />
+    </nav>
+    <div class="header__content">
+      <div class="header__content__section">
+        <h1 class="h1">Chris Lee Creative Design</h1>
+        <p class="caption">
+          Make designs specially illustrations, logos, UI/UX, apps & websites, social media,
+          magazines & banners.
+        </p>
+        <a href=""><button class="header__button">CONTACT US</button></a>
+      </div>
+      <div class="header__content__icons">
+        <a href="https://uk-ua.facebook.com/" target="_blank">
+          <img class="icon" src="{Facebook}" alt=""
+        /></a>
+        <a href="https://www.instagram.com/" target="_blank">
+          <img class="icon" src="{Instagram}" alt=""
+        /></a>
+        <a href=""> <img class="icon" src="{Be}" alt="" /></a>
+        <a href="https://www.instagram.com/" target="_blank">
+          <img class="icon" src="{Twitter}" alt=""
+        /></a>
+        <a href="https://www.pinterest.co.uk/" target="_blank">
+          <img class="icon" src="{Pinterest}" alt=""
+        /></a>
+      </div>
+    </div>
+  </div>
+</header>
+
+<style lang="scss"></style>
